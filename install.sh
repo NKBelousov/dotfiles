dc0375f10c274e9895eac0fbcd0dd11ec9ba0e7c
NKBelousov <belousovnikita92@gmail.com>
2016-10-17 09:03:34 +0300

    automatically run Vundle update

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@
 
 find . -maxdepth 1 -type f | grep -v 'install' | xargs -L 1 -I {} cp {} ~/{}
 mkdir -p ~/.vim/UltiSnips/ && cp -r ./UltiSnips/ ~/.vim/
+vim +PluginInstall +PluginClean +PluginUpdate +qall
