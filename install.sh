364db914af61f668a46b9f3f710757315b082882
NKBelousov <belousovnikita92@gmail.com>
2017-05-05 17:41:51 +0300

    upgrade vim-plug via install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 
 find . -maxdepth 1 -type f | grep -v 'install' | xargs -L 1 -I {} cp {} ~/{}
 mkdir -p ~/.vim/UltiSnips/ && cp -r ./UltiSnips/ ~/.vim/
-vim +PlugInstall +PlugClean +PlugUpdate +qall
+vim +PlugInstall +PlugClean +PlugUpdate +PlugUpgrade +qall
