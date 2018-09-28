7802337dcc575227912f8f59fb5832693bba3ded
NKBelousov <belousovnikita92@gmail.com>
2018-09-28 20:59:35 +0300

    add pomodoro aliases

--- .bash/functions.sh
+++ .bash/functions.sh
@@ -50,6 +50,30 @@ daily(){
   cat ~/.config/dconf/palette | dconf load /org/gnome/terminal/ | dconf update;
 }
 
+pomodoro(){
+  if [[ -z $1 ]]; then
+    echo "Specify duration"
+    return 1
+  fi
+  if [[ -z $2 ]]; then
+    echo "Specify message"
+    return 1
+  fi
+  sleep $1 && notify-send $2 &>/dev/null &disown;
+}
+
+pomodoro-work(){
+  pomodoro 1500 "Start working!"
+}
+
+pomodoro-short(){
+  pomodoro 300 "Short break"
+}
+
+pomodoro-long(){
+  pomodoro 3600 "Long break"
+}
+
 nvm-check(){
   if ! [ -x "$(command -v nvm)" ]; then
     nvm-install
