0eca55f97b238fad23e78d67d64ff3d60659d72a
NKBelousov <belousovnikita92@gmail.com>
2018-10-01 19:57:03 +0300

    always hide gnome-terminal menubar

--- .bash/aliases.sh
+++ .bash/aliases.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 alias bye="shutdown -h now"
+alias gnome-terminal="gnome-terminal --hide-menubar"
 alias ll="ls -alh --group-directories-first"
 alias resource="source ~/.bashrc"
 alias svim="sudo -e"
