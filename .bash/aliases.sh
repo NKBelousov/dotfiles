debad875c9ee87a29f671e6ad96c8b2aa53ce2ec
NKBelousov <belousovnikita92@gmail.com>
2018-10-02 23:00:47 +0300

    use colorized output for ll

--- .bash/aliases.sh
+++ .bash/aliases.sh
@@ -2,7 +2,7 @@
 
 alias bye="shutdown -h now"
 alias gnome-terminal="gnome-terminal --hide-menubar"
-alias ll="ls -alh --group-directories-first"
+alias ll="ls -alh --group-directories-first --color=auto"
 alias resource="source ~/.bashrc"
 alias svim="sudo -e"
 alias vi="vim"
