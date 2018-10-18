0ed4412698bb109c0bbee8a4646872555b4a12c0
NKBelousov <belousovnikita92@gmail.com>
2018-10-19 02:21:43 +0300

    add mkdir alias

--- .bash/aliases.sh
+++ .bash/aliases.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 alias bye="shutdown -h now"
-alias t="gnome-terminal --hide-menubar"
 alias ll="ls -alh --group-directories-first --color=auto"
+alias mkdir="mkdir -pv"
 alias resource="source ~/.bashrc"
 alias svim="sudo -e"
+alias t="gnome-terminal --hide-menubar"
 alias vi="vim"
