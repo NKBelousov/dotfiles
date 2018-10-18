db7ec352fb8940bd95c5c6f03926de8ad3c3979f
NKBelousov <belousovnikita92@gmail.com>
2018-10-19 02:16:01 +0300

    change gnome-terminal alias

--- .bash/aliases.sh
+++ .bash/aliases.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 alias bye="shutdown -h now"
-alias gnome-terminal="gnome-terminal --hide-menubar"
+alias t="gnome-terminal --hide-menubar"
 alias ll="ls -alh --group-directories-first --color=auto"
 alias resource="source ~/.bashrc"
 alias svim="sudo -e"
