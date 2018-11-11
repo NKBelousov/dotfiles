7b655bc256d59769fe9dbc4bff82b11d76784573
NKBelousov <belousovnikita92@gmail.com>
2018-11-11 03:47:05 +0300

    add git rbx alias

--- .bash/git.sh
+++ .bash/git.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 git-rebase-exec(){
-  if [ $# -ne 2 ]; then
-    echo "git-rebase-exec usage: commit command-to-execute"
+  if [ $# -lt 2 ]; then
+    echo "git rbx usage: commit command-to-execute"
     echo "    commit                rebase starting point"
     echo "    command               command to execute on each commit" 
     return 1
