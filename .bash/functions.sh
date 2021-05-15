79c5290080fe95bfbe02a762192cdff0c9669aea
NKBelousov <belousovnikita92@gmail.com>
2021-05-15 22:43:45 +0300

    update packages list

--- .bash/functions.sh
+++ .bash/functions.sh
@@ -19,19 +19,15 @@ daily(){
   "git"
   "htop"
   "iotop"
-  "snapd"
   "thunar"
   "tmux"
   "vim"
-  "xfce4"
-  "xfce4-terminal"
   )
   sudo apt update --yes;
   sudo apt full-upgrade --yes;
   sudo apt autoremove --yes;
   sudo apt autoclean --yes;
   sudo apt install ${essential[@]};
-  sudo snap install rg;
   nvm-check;
   vim +PlugInstall +PlugClean +PlugUpdate +PlugUpgrade +qall;
   notify-send "Ready to work!";
